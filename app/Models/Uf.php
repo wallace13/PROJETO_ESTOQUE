b5537cc2db40fb287c4836299a6d6708ba40b05a
Wallace Sales <49043987+wallace13@users.noreply.github.com>
2023-09-25 11:59:26 -0300

    Log-Uf

--- app/Models/Uf.php
+++ app/Models/Uf.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\ActivityLogger;
 
 class Uf extends Model
 {
@@ -16,6 +17,7 @@ class Uf extends Model
     | GLOBAL VARIABLES
     |--------------------------------------------------------------------------
     */
+    protected static $logName = 'ufs';
 
     protected $table = 'ufs';
     // protected $primaryKey = 'id';
@@ -29,6 +31,35 @@ class Uf extends Model
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
+
+    protected static function boot()
+    {
+        parent::boot();
+        // Exemplo de registro de atividade no método 'created'
+        static::created(function ($uf) {
+            $causador = backpack_auth()->user();
+            $eventName = 'created';
+            $descricao = "Novo uf criado por {$causador->name}";
+            ActivityLogger::logActivity($uf, $eventName, $causador, $descricao,static::$logName,$uf->attributes);
+        });
+
+        // Exemplo de registro de atividade no método 'updated'
+        static::updated(function ($uf) {
+            $causador = backpack_auth()->user();
+            $eventName = 'updated';
+            $descricao = "UF atualizado por {$causador->name}";
+            ActivityLogger::logActivity($uf, $eventName, $causador, $descricao,static::$logName,$uf->attributes);
+        });
+
+        // Exemplo de registro de atividade no método 'deleted'
+        static::deleted(function ($uf) {
+            $causador = backpack_auth()->user();
+            $eventName = 'deleted';
+            $descricao = "UF excluído por {$causador->name}";
+            ActivityLogger::logActivity($uf, $eventName, $causador, $descricao,static::$logName,$uf->attributes);
+        });
+
+    }
 
     /*
     |--------------------------------------------------------------------------
