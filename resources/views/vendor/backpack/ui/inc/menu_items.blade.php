3ea42061288746e247d5ab1cb60fd2320687ee7b
wallace13 <wsalesg@hotmail.com>
2023-12-26 17:11:44 -0300

    Correção Menu

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -5,4 +5,4 @@
 <x-backpack::menu-item title="Unidade de Fornecimento" icon="la la-balance-scale-left" :link="backpack_url('uf')" />
 <x-backpack::menu-item title="Registro de Entradas" icon="la la-cart-plus" :link="backpack_url('entrada')" />
 <x-backpack::menu-item title="Registro de Saidas" icon="la la-cart-arrow-down" :link="backpack_url('saida')" />
-<x-backpack::menu-item title="Activitylogs" icon="la la-question" :link="backpack_url('activitylog')" />
+<x-backpack::menu-item title="Atividades de Log" icon="la la-question" :link="backpack_url('activitylog')" />
